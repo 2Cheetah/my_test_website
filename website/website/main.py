4564a39c4b89e5502f3dbfd185e6e9db058d026f
Denis Safiullin <fckninja@gmail.com>
2024-01-28 11:09:58 +0800

    updated .gitignore, added basic webapp

---
+++ website/website/main.py
@@ -0,0 +1,22 @@
+'''FastAPI app and its handlers'''
+import uvicorn
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    '''root handler'''
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    '''/items handler'''
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app=app, host='0.0.0.0', port=8000)
